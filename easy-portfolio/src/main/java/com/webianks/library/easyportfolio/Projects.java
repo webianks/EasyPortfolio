ecc667781dd5d2d2cdced6da01fb2b5e27d2f065
webianks <webianks@gmail.com>
2017-05-03 11:22:51 +0530

    Recyclerview adapter complete

---
+++ easy-portfolio/src/main/java/com/webianks/library/easyportfolio/Projects.java
@@ -0,0 +1,37 @@
+package com.webianks.library.easyportfolio;
+
+/**
+ * Created by R Ankit on 03-05-2017.
+ */
+
+public class Projects {
+
+    private String projectName;
+    private String projectDesc;
+    private String projectLink;
+
+
+    public void setProjectDesc(String projectDesc) {
+        this.projectDesc = projectDesc;
+    }
+
+    public void setProjectLink(String projectLink) {
+        this.projectLink = projectLink;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getProjectDesc() {
+        return projectDesc;
+    }
+
+    public String getProjectLink() {
+        return projectLink;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+}
--- easy-portfolio/src/main/java/com/webianks/library/easyportfolio/ProjectsRecyclerAdapter.java
+++ easy-portfolio/src/main/java/com/webianks/library/easyportfolio/ProjectsRecyclerAdapter.java
@@ -1,8 +1,13 @@
 package com.webianks.library.easyportfolio;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
 
 /**
  * Created by R Ankit on 03-05-2017.
@@ -10,27 +15,44 @@ import android.view.ViewGroup;
 
 public class ProjectsRecyclerAdapter extends RecyclerView.Adapter<ProjectsRecyclerAdapter.VH> {
 
-    public ProjectsRecyclerAdapter() {
+    private Context context;
+    private List<Projects> projectsList;
+
+    public ProjectsRecyclerAdapter(Context context,List<Projects> projectsList) {
+        this.context = context;
+        this.projectsList = projectsList;
     }
 
     @Override
     public VH onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+
+        View view = LayoutInflater.from(context).inflate(R.layout.single_project_view,parent,false);
+        return new VH(view);
     }
 
     @Override
     public void onBindViewHolder(VH holder, int position) {
 
+        holder.title.setText(projectsList.get(position).getProjectName());
+        holder.desc.setText(projectsList.get(position).getProjectDesc());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return (projectsList != null)? projectsList.size() : 0;
     }
 
-    public class VH extends RecyclerView.ViewHolder {
-        public VH(View itemView) {
+    class VH extends RecyclerView.ViewHolder {
+
+        private TextView title;
+        private TextView desc;
+
+        VH(View itemView) {
             super(itemView);
+
+            title = (TextView) itemView.findViewById(R.id.projectName);
+            desc = (TextView) itemView.findViewById(R.id.projectDesc);
+
         }
     }
 }
